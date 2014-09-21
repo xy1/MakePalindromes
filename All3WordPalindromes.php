3846939b4fd66a4b0ada55c8d54b8061ad614543
xy1 <mattscandale@hotmail.com>
2014-09-21 10:00:09 -0500

    Update All3WordPalindromes.php

--- All3WordPalindromes.php
+++ All3WordPalindromes.php
@@ -7,7 +7,7 @@
  */
 
 /* Settings */
-define('MAX_WORD_LENGTH', 5);
+define('MAX_WORD_LENGTH', 4);
 
 /* Load word list from text file into array */
 $words = file('WordsList.txt');
@@ -21,14 +21,15 @@ for ($i = 0; $i < $word_count; $i++) {
 $words = $temp_words;
 $word_count = count($words);
 
-for ($i = 0; $i < $word_count; $i++) {
+for ($i = 0; $i < $word_count; $i++) {  // iterate word list for 1st word
         $this_word = $words[$i];
-        for ($j = 0; $j < $word_count; $j++) {
-                if ($j != $i) $second_word = $words[$j];
+
+        for ($j = 0; $j < $word_count; $j++) {  // iterate word list for 2nd word
+                $second_word = $words[$j];
                 $search_for_words = strrev($this_word . $second_word);
-                if (in_array($search_for_words, $words)) 
-                        echo $this_word . " " . $second_word . " " . $search_for_words . "\n";
-        }       
+                if (in_array($search_for_words, $words))  // look for 3rd word that matches first 2
+                        echo $this_word . " " . $second_word . " " . $search_for_words . "\n";  // show if found
+        }
 }
-        
+
 ?>
