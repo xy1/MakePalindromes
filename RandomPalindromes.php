da7773d4a339cfb3dbc332effebf2a28b7c03c87
xy1 <mattscandale@hotmail.com>
2014-09-21 10:04:34 -0500

    Update RandomPalindromes.php

--- RandomPalindromes.php
+++ RandomPalindromes.php
@@ -17,13 +17,14 @@ $words = file('WordsList.txt');
 /* clean up words */
 $word_count = count($words);
 for ($i = 0; $i < $word_count; $i++) {
-        $words[$i] = trim(strtolower($words[$i]));
-        if (strlen($words[$i]) <= constant('MAX_WORD_LENGTH') && $words[$i]) $temp_words[] = $words[$i];
+        $words[$i] = trim(strtolower($words[$i]));  // trim and convert to lower case
+        if (strlen($words[$i]) <= constant('MAX_WORD_LENGTH') && $words[$i])  // remove lengthy words to improve performance
+                $temp_words[] = $words[$i];
 }
 $words = $temp_words;
 
-for ($i = 1; $i <= constant('ATTEMPTS'); $i++) {
-        
+for ($i = 1; $i <= constant('ATTEMPTS'); $i++) {  // iterate defined number of attempts
+
         /* Build sentence from N randomly-selected words */
         $sentence = "";
         $deblanked_sentence = "";
@@ -31,7 +32,7 @@ for ($i = 1; $i <= constant('ATTEMPTS'); $i++) {
                 $word = $words[array_rand($words)];
                 $sentence .= $word . " ";
                 $deblanked_sentence .= $word;
-        }       
+        }
 
         /* Check for palindrome and show if so */
         if (strrev($deblanked_sentence) == $deblanked_sentence) echo $sentence . "\n";
