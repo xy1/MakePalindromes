bff6668f05f941814b07bc3623762ea7f7da3a31
xy1 <mattscandale@hotmail.com>
2014-09-21 09:44:39 -0500

    Create RandomPalindromes.php

---
+++ RandomPalindromes.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * RandomPalindromes.php
+ *
+ * Generate palindromes randomly from English word list
+ */
+
+/* Settings */
+define('MAX_WORD_LENGTH', 3);
+define('ATTEMPTS', 10000000);
+define('WORDS_IN_SENTENCE', 3);
+
+/* Load word list from text file into array */
+$words = file('WordsList.txt');
+
+/* clean up words */
+$word_count = count($words);
+for ($i = 0; $i < $word_count; $i++) {
+        $words[$i] = trim(strtolower($words[$i]));
+        if (strlen($words[$i]) <= constant('MAX_WORD_LENGTH') && $words[$i]) $temp_words[] = $words[$i];
+}
+$words = $temp_words;
+
+for ($i = 1; $i <= constant('ATTEMPTS'); $i++) {
+        
+        /* Build sentence from N randomly-selected words */
+        $sentence = "";
+        $deblanked_sentence = "";
+        for ($j = 1; $j <= constant('WORDS_IN_SENTENCE'); $j++) {
+                $word = $words[array_rand($words)];
+                $sentence .= $word . " ";
+                $deblanked_sentence .= $word;
+        }       
+
+        /* Check for palindrome and show if so */
+        if (strrev($deblanked_sentence) == $deblanked_sentence) echo $sentence . "\n";
+}
+
+?>
